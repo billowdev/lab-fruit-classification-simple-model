fa038b88c54e50067fb883f2f9d2472cc9c41f82
lacakp <lacakp.contact@gmail.com>
2021-11-04 22:08:02 +0700

    update code

--- GUI_abt_model_mobileNetV2_mobileNetV2.py
+++ GUI_abt_model_mobileNetV2_mobileNetV2.py
@@ -1,3 +1,4 @@
+from ctypes import windll
 from tkinter import *
 from tkinter import filedialog
 import tkinter as tk
@@ -11,6 +12,8 @@ from contextlib import suppress
 
 from keras import Model
 from keras.layers import Dense
+
+windll.shcore.SetProcessDpiAwareness(1)
 
 # Custom model
 abt_model = load_model("./Model/apple_banana_tomato_model")
@@ -38,15 +41,13 @@ gui.geometry("1080x650")
 # input_imgage = None
 
 def show_image():
+	global fln
 	fln = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select Image File", filetypes=(("JPG File", "*.jpg"), ("PNG File", "*.png"), ("All Files", "*.")))
 	img = Image.open(fln)
 	img.thumbnail((400, 400)) # dimention image resize
 	img = ImageTk.PhotoImage(img)
 	lbl.configure(image=img)
 	lbl.image = img
-
-	global input_imgage
-	input_imgage = cv2.imread(fln)
 
 
 def abt_predict(image):
@@ -55,30 +56,39 @@ def abt_predict(image):
 	Prediction Function 
 	args: image (<class 'numpy.ndarray'>)
 	"""
+
 	resized = cv2.resize(image, (224, 224))
 	img_array = tf.keras.preprocessing.image.img_to_array(resized)
 	img_array = tf.expand_dims(img_array, 0)
 	pred = abt_model.predict(img_array)
+	global score
 	score = pred[0]
-	classes = abt_model.predict(img_array/255.0, batch_size=32, verbose=0)
+	# print(abt_model.predict(img_array/255.0, batch_size=32, verbose=0))
+
 	label = np.argmax(pred[0])
 	if label == 0:
-		t0 = "ผลลัพธ์การทำนาย คือ ... "
+		t0 = "ผลลัพธ์การทำนาย คือ ... Apple"
+	if label == 1:
+		t0 = "ผลลัพธ์การทำนาย คือ ... Banana"
+	else:
+		t0 = "ผลลัพธ์การทำนาย คือ ... Tomato"
 	
 	t1 = "prediction score: Apple {:.2f} %".format(score[0]*100)
 	t2 = "prediction score: Banana {:.2f} %".format(score[1]*100)
 	t3 = "prediction score: Tomato {:.2f} %".format(score[2]*100)
 
-	alltext = f"{t1} \n {t2} \n {t3} \n"
+	alltext = f"{t0} \n {t1} \n {t2} \n {t3} \n -------------------"
 
 	return alltext
 
 def prediction():
-	# with suppress()
-	res = abt_predict(input_imgage)
+	input_image = cv2.imread(fln)
+	res = abt_predict(input_image)
 	res_text.insert(tk.END, res)
 
+
 def clear_text():
+	lbl.configure(image="")
 	res_text.delete('1.0', END)
 
 # Create text widget and specify size.
